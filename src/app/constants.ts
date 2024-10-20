b948c8cd10c4bbd271108f382db898ce38cd967c
Ashok IT <30829130+ashokitschool@users.noreply.github.com>
2024-10-20 10:46:50 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a0214571ffcf8436aabbc5cd8c03c3a5-1667859194.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a5a0029d2571e4f3495ab51500f19e55-1888408116.ap-south-1.elb.amazonaws.com',
 };
